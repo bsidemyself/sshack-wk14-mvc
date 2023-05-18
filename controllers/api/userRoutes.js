c2c075c769089000b374bb4ea220bf5d50cedfc5
bsidemyself <shackletron@gmail.com>
2023-05-18 16:42:44 -0700

    with handlebars login not working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
       res
@@ -47,6 +47,7 @@ router.post('/login', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
 
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
